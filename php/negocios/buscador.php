cc79f376b87df030bcb919577b8b53c4885dcb78
Jozhi2 <josias_ortizg@hotmail.com>
2015-04-14 13:17:48 -0600

    rotacion de negocios buscar

--- php/negocios/buscador.php
+++ php/negocios/buscador.php
@@ -4,39 +4,68 @@
   include("connect_db.php");
   $result = mysqli_query($link, "SELECT * FROM negocios LEFT JOIN imagenesbss ON negocios.id_negocio = imagenesbss.id_bss WHERE namebss LIKE '%$buscar%'"); 
 
-      $cb=0;
-    if ($row6 = mysqli_fetch_array($result)){ 
-      do {
-              $cb=$cb+1;
-              $bss_mostrar[$cb]=$row6['namebss'];
-              $bss_mostrar1[$cb]=$row6['address'];
-              $bss_mostrar2[$cb]=$row6['descrip'];
-              $bss_mostrar3[$cb]=$row6['imagenbss'];
-          } while($row6=mysqli_fetch_array($result));
-          for ($i = 1; $i <=$cb; $i++) {
-                if(isset($bss_mostrar3[$i]))
-                {
-                    $bss_mostrarimg[$i]="../imagenesbss/".$bss_mostrar3[$i];
-                }else{
-                    $bss_mostrarimg[$i]= "../objetos/sinFoto.png";
-                }
+  $cb=0;
+  if ($row6 = mysqli_fetch_array($result)){ 
+    do 
+    {
+      $cb=$cb+1;
+      $bss_mostrar[$cb]=$row6['namebss'];
+      $bss_mostrar1[$cb]=$row6['address'];
+      $bss_mostrar2[$cb]=$row6['descrip'];
+      $bss_mostrar3[$cb]=$row6['imagenbss'];
+    } while($row6=mysqli_fetch_array($result));
+    for ($i = 1; $i <=$cb; $i++) 
+    {
+      if(isset($bss_mostrar3[$i]))
+        {
+          $bss_mostrarimg[$i]="../imagenesbss/".$bss_mostrar3[$i];
+        }else{
+          $bss_mostrarimg[$i]= "../objetos/sinFoto.png";
+        }
+    }
+  } 
 
-          }
-     } 
-
-       else { 
+  else { 
             echo "¡ No se ha encontrado ningún registro !"; 
-      } 
+  } 
+  for($a=1;$a<=$cb;$a++) 
+  {
+      $numra[$a]=rand(1,$cb);
+      if($a>1) 
+      {
+          for($x=1; $x<$a; $x++)
+          {
+            if($numra[$a]==$numra[$x]) 
+            { 
+                $a--; 
+                break; 
+            }
+          }
+      }
+  }
 
+  for ($k = 1; $k <=$cb; $k++) {
+      $bss_mostraral[$k]=$bss_mostrar[$numra[$k]];
+      $bss_mostrar1al[$k]=$bss_mostrar1[$numra[$k]];
+      $bss_mostrar2al[$k]=$bss_mostrar2[$numra[$k]];
+      $bss_mostrar3al[$k]=$bss_mostrar3[$numra[$k]];
+        if(isset($bss_mostrarimg[$k]))
+            {
+                $bss_mostrarimgal[$k]=$bss_mostrarimg[$numra[$k]];
+            }else{
+                $bss_mostrarimgal[$k]=$bss_mostrarimg[$numra[$k]];
+            }
+
+    }
 ?> 
 <script>
       var variableb = "<?php echo $cb; ?>" ;
       var number= parseInt(variableb);
       //var nombresNegocio= new Array();
-      nombresNegocio=<?php echo json_encode($bss_mostrar);?>;
-      direccionNegocio=<?php echo json_encode($bss_mostrar1);?>;
-      descripNegocio=<?php echo json_encode($bss_mostrar2);?>;
-      imagenNegocio=<?php echo json_encode($bss_mostrarimg);?>;
+      nombresNegocio=<?php echo json_encode($bss_mostraral);?>;
+      direccionNegocio=<?php echo json_encode($bss_mostrar1al);?>;
+      descripNegocio=<?php echo json_encode($bss_mostrar2al);?>;
+      imagenNegocio=<?php echo json_encode($bss_mostrarimgal);?>;
       var imgNegocio=new Array();
       var desNegocio=new Array();
       var dirNegocio=new Array();
